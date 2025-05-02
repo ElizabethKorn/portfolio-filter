e91a94df122fad87ffc240aaa182ea4f291d1175
Liza <lkorn036@gmail.com>
2025-05-02 13:43:31 -0500

    files added

---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Toolbar = ({ filters, selected, onSelectFilter }) => {
+  return (
+    <div className="toolbar">
+      {filters.map((filter) => (
+        <button
+          key={filter}
+          className={filter === selected ? "active" : ""}
+          onClick={() => onSelectFilter(filter)}
+        >
+          {filter}
+        </button>
+      ))}
+    </div>
+  );
+};
